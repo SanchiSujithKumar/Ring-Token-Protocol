0a4b77dbaeb60b6f5f0fd0e1b6734cfb596137f0
SanchiSujithKumar <sanchisujithkumar@gmail.com>
2024-06-08 14:40:25 +0530

    these are optimised queue files

---
+++ base_station.py
@@ -0,0 +1,93 @@
+import time
+import threading
+import socket
+import json
+import random
+
+deliver_msg = False
+msg = ""
+new_node = False
+i = 1
+
+def input(input_socket):
+    global deliver_msg, msg
+    while True:
+        # recieving the messages
+        if not deliver_msg:
+            message = input_socket.recv(1024)
+            input_socket.send('.'.encode())
+            msg = message
+            deliver_msg = True
+
+def deliver(neighbour):
+    global deliver_msg, msg, new_node, i
+    while True:
+        
+        if deliver_msg:
+            neighbour.send(msg)
+            ack = neighbour.recv(1024)
+            deliver_msg = False
+        
+        # expanding the ring
+        if new_node:
+            neighbour.send('cis'.encode())
+            ack = neighbour.recv(1024)
+            port = 12345 + i - 1
+            port = str(port)
+            neighbour.send(port.encode())
+            ack = neighbour.recv(1024)
+            neighbour.close()
+            new_node = False
+            break
+
+def output(output_socket):
+    global deliver_msg, msg, i, new_node
+
+    has_ring = False
+    while True:
+        # assigning the node the id
+        neighbour, addr = output_socket.accept()
+        port = 12345 + i
+        i = i + 1
+        port = str(port)
+        neighbour.send(port.encode())
+        ack = neighbour.recv(1024)
+
+        # releasing the token to the first node
+        if not has_ring:
+            input_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            host = 'localhost'
+            input_socket.connect((host, int(port)))
+            thread_input = threading.Thread(target=input, args=(input_socket,))
+            thread_input.start()
+            token = {
+                "is_taken": False,
+                "source_id": 0,
+                "destination_id": 0,
+                "num_of_packets": 0,
+                "ack": False,
+                "time_sent": None
+            }
+            token_string = json.dumps(token)
+            neighbour.send(token_string.encode())
+            ack = neighbour.recv(1024)
+            has_ring = True
+            
+        else:
+            new_node = True
+            thread_deliver.join()
+
+        thread_deliver = threading.Thread(target=deliver, args=(neighbour,))
+        thread_deliver.start()
+
+def main():
+    # server socket for incoming nodes to form the ring
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = 'localhost'
+    port = 12345
+    server_socket.bind((host, port))
+    server_socket.listen(5)
+    thread_output = threading.Thread(target=output, args=(server_socket,))
+    thread_output.start()
+
+main()
---
+++ node.py
@@ -0,0 +1,167 @@
+import time
+import threading
+import socket
+import json
+import random
+
+id = 0
+forward_packet = False
+packet = ""
+payload = []
+send_packet = False
+limit = 3
+tl = 0
+num_of_packets_to_send = 0
+
+def set_tl():
+    global tl
+    tl = 1
+    threading.Timer(10, reset_tl).start()
+    
+def reset_tl():
+    global tl
+    tl = 0
+
+def inputsocket(input_socket):
+    global forward_packet, packet, id, payload, send_packet, limit, tl, num_of_packets_to_send
+
+    token = {}
+    msg_to_be_recived = 0
+    start_time = 0
+    retransmission = 0
+
+    while True:
+        if not forward_packet:
+            message = input_socket.recv(1024)
+            
+            # expanding the ring
+            if message == b'cis':
+                input_socket.send('.'.encode())
+                host = 'localhost'
+                port = input_socket.recv(1024).decode()
+                input_socket.send('.'.encode())
+                input_socket.close()
+                input_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                input_socket.connect((host, int(port)))
+                continue
+
+            recived_packet = message.decode()
+
+            if not recived_packet.startswith("{") :
+                if msg_to_be_recived > 0 :
+                    print("\n[Message received] <node" + str(token["source_id"]) + "> : " + recived_packet)
+                    msg_to_be_recived -= 1
+                    if msg_to_be_recived == 0 :
+                        packet = json.dumps(token).encode()
+                        forward_packet = True
+                else :
+                    packet = message
+                    forward_packet = True
+                input_socket.send('.'.encode())
+                continue
+
+            token = json.loads(recived_packet)
+            if token["destination_id"] == id and token["source_id"] != 0:
+                x = random.random()
+                if x > 0.3:
+                    msg_to_be_recived = token["num_of_packets"]
+                    token["ack"] = True
+                    input_socket.send('.'.encode())
+                    continue
+                message = json.dumps(token).encode()
+
+            if not token["is_taken"] and send_packet and tl==0:
+                token["source_id"] = id
+                token["is_taken"] = True
+                print("\nHolding Token\n")
+                start_time = time.time()
+            
+            if token["is_taken"] and token["source_id"] == id:
+                if token["ack"] or retransmission == 2:
+                    print("\nRTT:", time.time() - token["time_sent"])
+                    token["num_of_packets"] = 0
+                    token["ack"] = False
+                    token["time_sent"] = None
+                    token["destination_id"] = 0
+                    for i in range(num_of_packets_to_send) :
+                        payload.remove(payload[0])
+                    num_of_packets_to_send = 0
+                    message = json.dumps(token).encode()
+                else:
+                    retransmission += 1
+                tht = time.time() - start_time
+                
+                if tht > limit or not len(payload):
+                    if tht > limit:
+                        print("\nToken Timeout")
+                    print("Token Holding Time:", tht)
+                    print()
+                    token["is_taken"] = False
+                    token["source_id"] = 0
+                    send_packet = False
+                    if len(payload):
+                        send_packet = True
+                    message = json.dumps(token).encode()
+                    set_tl()
+
+                if token["is_taken"] and len(payload):
+                    token["destination_id"] = int(payload[0][0])
+                    num_of_packets_to_send = 1
+                    while len(payload) > num_of_packets_to_send :
+                        if payload[num_of_packets_to_send][0] == payload[0][0] :
+                            num_of_packets_to_send += 1
+                        else :
+                            break
+                    token["num_of_packets"] = num_of_packets_to_send
+                    token["ack"] = False
+                    retransmission = 0
+                    token["time_sent"] = time.time()
+                    message = json.dumps(token).encode()
+                
+            packet = message
+            forward_packet = True
+            input_socket.send('.'.encode())
+
+def output(output_socket):
+    global forward_packet, packet, num_of_packets_to_send, payload
+    neighbour, addr = output_socket.accept()
+    while True:
+        if forward_packet:
+            neighbour.send(packet)
+            Ack = neighbour.recv(1024)
+            for i in range(num_of_packets_to_send):
+                neighbour.send(payload[i][1].encode())
+                Ack = neighbour.recv(1024)
+            forward_packet = False
+
+def main():
+    global forward_packet, packet, id, payload, send_packet
+
+    input_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = 'localhost'
+    port = 12345
+    input_socket.connect((host, port))
+
+    port = input_socket.recv(1024).decode()
+    output_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    output_socket.bind((host, int(port)))
+    output_socket.listen(1)
+    input_socket.send('.'.encode())
+
+    thread_output = threading.Thread(target=output, args=(output_socket,))
+    thread_output.start()
+    thread_input = threading.Thread(target=inputsocket, args=(input_socket,))
+    thread_input.start()
+
+    id = int(port) - 12345
+    print("Station: Node " + str(id))
+    while True:
+        if not send_packet:
+            inpl = int(input("\n**Enter the no. of packets you want to send:**\n"))
+            for i in range(inpl):
+                destination = int(input(f"Enter destination id for sending {i+1}st packet: "))
+                payl = input("Enter the message: ")
+                payload.append((destination, payl))
+            send_packet = True
+
+main()
