d9cf812ad09b2533b6beaa4a988506d4bf26bbe5
SanchiSujithKumar <sanchisujithkumar@gmail.com>
2024-06-11 00:33:04 +0530

    sample run to original protocol

--- src/node/node.py
+++ src/node/node.py
@@ -11,30 +11,27 @@ payload = []
 send_packet = False
 retransmission = 0
 start_time = 0
-limit = 4
-sent = 0
-start = 0
+limit = 3
 tl = 0
-nop = 0
 
 def set_tl():
     global tl
     tl = 1
-    threading.Timer(10, reset_tl).start()
+    threading.Timer(1, reset_tl).start()
     
 def reset_tl():
     global tl
     tl = 0
 
 def inputsocket(input_socket):
-    global forward_packet, packet, id, payload, send_packet, retransmission, start_time, limit, sent, start, tl, nop
-
+    global forward_packet, packet, id, payload, send_packet, retransmission, start_time, limit, tl
+    num_of_packets_deliverd = 0
+    total_time_taken = 0.0
     while True:
         if not forward_packet:
             message = input_socket.recv(1024)
             
             if message == b'cis':
-                # print("S1")
                 input_socket.send('.'.encode())
                 host = 'localhost'
                 port = input_socket.recv(1024).decode()
@@ -46,27 +43,28 @@ def inputsocket(input_socket):
             token = json.loads(message.decode())
             
             if token["destination_id"] == id and token["source_id"] != 0:
-                # print("S2")
                 x = random.random()
-                # print(x)
                 x = 0.4
                 if x > 0.3:
                     print("\n[Message received] <node" + str(token["source_id"]) + "> : " + token["payload"])
                     token["ack"] = True
                 message = json.dumps(token).encode()
             
-            if token["is_taken"] and token["source_id"] == id and sent == 1:
-                # print("S3")
+            if not token["is_taken"] and send_packet and tl==0:
+                token["source_id"] = id
+                token["is_taken"] = True
+                print("\nHolding Token\n")
+                start_time = time.time()
+            
+            if token["is_taken"] and token["source_id"] == id:
                 if token["ack"] or retransmission == 2:
                     print("\nRTT:", time.time() - token["time_sent"])
-                    # print(retransmission)
                     token["payload"] = ""
                     token["ack"] = False
                     token["time_sent"] = None
-                    token["destination_id"] = -1
-                    token["source_id"] = -1
+                    token["destination_id"] = 0
                     payload.remove(payload[0])
-                    sent = 0
+                    num_of_packets_deliverd += 1
                     message = json.dumps(token).encode()
                 else:
                     retransmission += 1
@@ -75,33 +73,24 @@ def inputsocket(input_socket):
                 if tht > limit or not len(payload):
                     if tht > limit:
                         print("\nToken Timeout")
-                    start = 0
                     print("Token Holding Time:", tht)
+                    total_time_taken += tht
+                    print("avg time to deliver per packet:", total_time_taken / num_of_packets_deliverd)
                     print()
                     token["is_taken"] = False
                     send_packet = False
+                    if len(payload):
+                        send_packet = True
+                    message = json.dumps(token).encode()
                     set_tl()      
-            
-            if not token["is_taken"] and send_packet and tl==0:
-                # print("S4")
-                token["is_taken"] = True
-                print("\nHolding Token\n")
-                
-            if token["is_taken"] and len(payload) and sent == 0:
-                # print("S5")
-                if start == 0:
-                    while(len(payload)!=nop):
-                        pass
-                    start_time = time.time()
-                    start = 1
-                token["is_taken"] = True
-                token["source_id"] = id
-                token["destination_id"] = int(payload[0][0])  # Ensure correct type
-                token["payload"] = payload[0][1]
-                token["ack"] = False
-                sent = 1
-                token["time_sent"] = time.time()
-                message = json.dumps(token).encode()
+
+                if token["is_taken"] and len(payload):
+                    token["destination_id"] = int(payload[0][0])  # Ensure correct type
+                    token["payload"] = payload[0][1]
+                    token["ack"] = False
+                    retransmission = 0
+                    token["time_sent"] = time.time()
+                    message = json.dumps(token).encode()
                 
             input_socket.send('.'.encode())
             packet = message
@@ -117,7 +106,7 @@ def output(output_socket):
             forward_packet = False
 
 def main():
-    global forward_packet, packet, id, payload, send_packet, nop
+    global forward_packet, packet, id, payload, send_packet
 
     input_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = 'localhost'
@@ -139,13 +128,23 @@ def main():
     print("Station: Node " + str(id))
     while True:
         if not send_packet:
-            nop = len(payload)
             inpl = int(input("\n**Enter the no. of packets you want to send:**\n"))
-            nop += inpl
-            for i in range(inpl):
-                destination = int(input(f"Enter destination id for sending {i+1}st packet: "))
-                payl = input("Enter the message: ")
-                payload.append((destination, payl))
+            if inpl == 0:
+                destination = 0
+                for __ in range (100):
+                    destination += 1
+                    if destination == id :
+                        destination += 1
+                    if destination > 5 :
+                        destination %= 5
+                        if id == 1 :
+                            destination += 1
+                    payload.append((destination, str("sample text")))
+            else: 
+                for i in range(inpl):
+                    destination = int(input(f"Enter destination id for sending {i+1}st packet: "))
+                    payl = input("Enter the message: ")
+                    payload.append((destination, payl))
             send_packet = True
 
 main()
